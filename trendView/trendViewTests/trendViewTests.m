aa7f30ca84c55120ba70bca4acded3e21e95fa26
Jon Rubin <jb.rubin@gmail.com>
2013-01-19 13:27:36 -0500

    Adds a unit test to successfully load the sample weight data.

--- trendView/trendViewTests/trendViewTests.m
+++ trendView/trendViewTests/trendViewTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "trendViewTests.h"
+#import "CHCSVParser.h"
 
 @implementation trendViewTests
 
@@ -24,9 +25,34 @@
     [super tearDown];
 }
 
-- (void)testExample
+/* Tests that a sample .csv file can successfully be read */
+- (void)testCSVParser
 {
-    STFail(@"Unit tests are not implemented yet in trendViewTests");
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"weightbot_data" ofType:@"csv"];
+    
+    NSArray *rows = [NSArray arrayWithContentsOfCSVFile:path];
+    
+    STAssertNotNil(rows, @"Error parsing file.");
+    
+    // For thoroughness, let's check 3 dates -- the first, the middle, and the last.
+
+    // 2012-01-01, 87.3, 192.5
+    NSArray * firstRow = rows[1]; // Index 0 is the column headers
+    STAssertTrue([firstRow[0] isEqualToString:@"2012-01-01"], @"Row 1's date is %@, not 2012-01-01", firstRow[0]);
+    STAssertTrue([firstRow[1] isEqualToString:@"87.3"], @"Row 1's kilos is %@, not 87.3", firstRow[1]);
+    STAssertTrue([firstRow[2] isEqualToString:@"192.5"], @"Row 1's pounds is %@, not 192.5", firstRow[2]);
+    
+    // 2012-03-28, 78.0, 172.0
+    NSArray * middleRow = rows[88];
+    STAssertTrue([middleRow[0] isEqualToString:@"2012-03-28"], @"Row 89's date is %@, not 2012-03-28", middleRow[0]);
+    STAssertTrue([middleRow[1] isEqualToString:@"78.0"], @"Row 2's kilos is %@, not 78.0", middleRow[1]);
+    STAssertTrue([middleRow[2] isEqualToString:@"172.0"], @"Row 2's pounds is %@, not 172.0", middleRow[2]);
+    
+    // 2013-01-19, 94.1, 207.4
+    NSArray * lastRow = rows[178];
+    STAssertTrue([lastRow[0] isEqualToString:@"2013-01-19"], @"Row 178's date is %@, not 2013-01-19", lastRow[0]);
+    STAssertTrue([lastRow[1] isEqualToString:@"94.1"], @"Row 178's kilos is %@, not 94.1", lastRow[1]);
+    STAssertTrue([lastRow[2] isEqualToString:@"207.4"], @"Row 178's pounds is %@, not 207.4", lastRow[2]);
 }
 
 @end
