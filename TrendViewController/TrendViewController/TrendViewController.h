3816eeb6370d6e3faf14513363ec979c0224c47e
Jon Rubin <jb.rubin@gmail.com>
2015-07-11 10:54:35 -0400

    Adds an empty framework project for the trend view controller.

---
+++ TrendViewController/TrendViewController/TrendViewController.h
@@ -0,0 +1,19 @@
+//
+//  TrendViewController.h
+//  TrendViewController
+//
+//  Created by Jonathon Rubin on 7/11/15.
+//  Copyright (c) 2015 Jonathon Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrendViewController.
+FOUNDATION_EXPORT double TrendViewControllerVersionNumber;
+
+//! Project version string for TrendViewController.
+FOUNDATION_EXPORT const unsigned char TrendViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrendViewController/PublicHeader.h>
+
+
