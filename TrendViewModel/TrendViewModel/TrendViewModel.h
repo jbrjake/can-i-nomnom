2e1f5d1bac1447fbc42f1efae1981d927c01a32b
Jon Rubin <jb.rubin@gmail.com>
2015-07-11 10:53:24 -0400

    Adds an empty framework project for the trend view model.

---
+++ TrendViewModel/TrendViewModel/TrendViewModel.h
@@ -0,0 +1,19 @@
+//
+//  TrendViewModel.h
+//  TrendViewModel
+//
+//  Created by Jonathon Rubin on 7/11/15.
+//  Copyright (c) 2015 Jonathon Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrendViewModel.
+FOUNDATION_EXPORT double TrendViewModelVersionNumber;
+
+//! Project version string for TrendViewModel.
+FOUNDATION_EXPORT const unsigned char TrendViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrendViewModel/PublicHeader.h>
+
+
