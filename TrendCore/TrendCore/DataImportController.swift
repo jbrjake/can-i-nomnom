9df81824fe9c1b84acad59e2fb1e784eb08f2e3d
Jon Rubin <jb.rubin@gmail.com>
2015-07-17 22:28:37 -0400

    Starts to stub out the data importer classes.

---
+++ TrendCore/TrendCore/DataImportController.swift
@@ -0,0 +1,18 @@
+//
+//  DataImportController.swift
+//  TrendCore
+//
+//  Created by Jonathon Rubin on 7/17/15.
+//  Copyright (c) 2015 Jonathon Rubin. All rights reserved.
+//
+
+import Foundation
+
+enum TrendCoreImporterType {
+    case Dummy
+    case HealthKit
+}
+
+class DataImportController {
+    
+}
---
+++ TrendCore/TrendCore/DataImporter.swift
@@ -0,0 +1,13 @@
+//
+//  DataImporter.swift
+//  TrendCore
+//
+//  Created by Jonathon Rubin on 7/17/15.
+//  Copyright (c) 2015 Jonathon Rubin. All rights reserved.
+//
+
+import Foundation
+
+class DataImporter {
+    
+}
